b244ffaeb54951038e4322c1eeeff8219ab0f740
Yurgenich <yurakarbushev@gmail.com>
2024-02-28 00:39:02 +0700

    Add home work 2

--- Home_work_1/app.py
+++ Home_work_1/app.py
@@ -1,52 +1,104 @@
-from flask import Flask, render_template
+import secrets
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+app.secret_key = secrets.token_hex(16)
+target_metadata = db.metadata
 
-@app.route('/static/<path:path>')
-def static_file(path):
-    return app.send_static_file(path)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
 
 
-@app.route('/')
+# Определение модели пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
+# Создание всех таблиц в базе данных
+with app.app_context():
+    db.create_all()
+
+app.config['STORE'] = '"Краски стиля"'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    tab = app.config['STORE']
+    title = 'Главная'
+    context = ''
+    category = None
+
+    return render_template('base.html', category=category, tab=tab, title=title)
+
+
+@app.route('/login/')
+def login():
+    tab = app.config['STORE']
+    title = 'Главная'
+    context = 'Вход'
+    category = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('login.html', category=category, tab=tab+title, title=title, context=context)
+
+
+@app.route('/index/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    tab = app.config['STORE']
+    title = 'Главная'
+    category = None
+    if request.method == 'POST':
+        username = request.form['username']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('index.html', category=category, tab=tab, title=title)
 
 
-@app.route('/clothing')
-def clothing():
-    category = {
-        'title': 'Clothing',
-        'products': [
-            {'id': 1, 'name': 'T-shirt', 'price': 20, 'image': 'tshirt.png'},
-            {'id': 2, 'name': 'Jeans', 'price': 50, 'image': 'jeans.png'}
-        ]
-    }
-    return render_template('category.html', category=category)
+@app.route('/logout/')
+def logout():
+    """Выход из системы"""
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
-@app.route('/shoes')
-def shoes():
-    category = {
-        'title': 'Shoes',
-        'products': [
-            {'id': 3, 'name': 'Sneakers', 'price': 70, 'image': 'sneakers.png'},
-            {'id': 4, 'name': 'Boots', 'price': 100, 'image': 'boots.png'}
-        ]
-    }
-    return render_template('category.html', category=category)
+@app.route('/product/')
+def product():
+    tab = app.config['STORE']
+    title = 'Главная'
+    category = 'Товары'
+    products = [
+        {'name': 'Футболка', 'price': 20, 'image': 'tshirt.png', 'category': 'Одежда'},
+        {'name': 'Джинсы', 'price': 50, 'image': 'jeans.png', 'category': 'Одежда'},
+        {'name': 'Кофта', 'price': 30, 'image': 'sweater.png', 'category': 'Одежда'},
+        {'name': 'Платье', 'price': 60, 'image': 'dress.png', 'category': 'Одежда'},
 
-
-@app.route('/products/<int:product_id>')
-def product(product_id):
-    product = {
-        'id': product_id,
-        'name': 'Sample Product',
-        'price': 50,
-        'description': 'This is a sample product.',
-        'image': 'image/sample_product.png'  # Путь к изображению товара
-    }
-    return render_template('product.html', product=product)
+    ]
+    return render_template('product.html', category=category, tab=tab, title=title, products=products)
 
 
 if __name__ == '__main__':
---
+++ alembic/env.py
@@ -0,0 +1,56 @@
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+# Этот блок добавляется для импорта ваших моделей и используемой вами базы данных SQLAlchemy
+import sys
+import os
+
+sys.path.append(os.getcwd())
+from Home_work_1.app import db
+
+# Это нужно для корректной работы Alembic
+config = context.config
+fileConfig(config.config_file_name)
+
+# Укажите объект метаданных вашей базы данных SQLAlchemy
+target_metadata = db.metadata
+
+# Здесь необходимо передать объект метаданных в Alembic
+config.set_main_option('sqlalchemy.url', str(db.engine.url))
+
+
+def run_migrations_offline():
+    """Эта функция применяет миграции в автономном режиме."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url, target_metadata=target_metadata, literal_binds=True)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Эта функция применяет миграции в режиме онлайн."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
